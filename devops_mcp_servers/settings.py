72022275c6f837c5af3692fe1d8a5b2673384852
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-15 09:37:17 +0800

    修改白名单列表

--- devops_mcp_servers/settings.py
+++ devops_mcp_servers/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*538!(b@vxf@#wo(#xpk+yuvca!a65^t^jiqgq##7f1=bxytxg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
