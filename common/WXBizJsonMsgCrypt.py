e68380322ec8e9d0eb39adcdfbf2690e981caeb2
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-15 16:17:16 +0800

    打印解密内容

--- common/WXBizJsonMsgCrypt.py
+++ common/WXBizJsonMsgCrypt.py
@@ -192,6 +192,7 @@ class Prpcrypt(object):
             json_len = socket.ntohl(struct.unpack("I", content[: 4])[0])
             json_content = content[4: json_len + 4].decode('utf-8')
             from_receiveid = content[json_len + 4:].decode('utf-8')
+            logger.debug(f"解密id结果: {from_receiveid}")
         except Exception as e:
             logger.error(e)
             return ierror.WXBizMsgCrypt_IllegalBuffer, None
