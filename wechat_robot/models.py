7dd3d39b7e0cdf179cb297e26268620920d5a3be
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-15 17:01:38 +0800

    修改序列化结构

--- wechat_robot/models.py
+++ wechat_robot/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 
 class WechatRobotQuestionBase(models.Model):
-    msgid = models.CharField(max_length=36, unique=True, primary_key=True)
-    aibotid = models.CharField(max_length=36, name="机器人id", default="", null=False)
-    chatid = models.CharField(max_length=36, name="聊天id", default="", null=False)
-    chattype = models.CharField(max_length=36, choices=(("group", "群"),("single", "个人")), default="group", null=False)
-    chat_from = models.JSONField(default=dict(), null=False, blank=False,db_column="from")
+    msgid = models.CharField(max_length=36, unique=True, primary_key=True, verbose_name="消息id")
+    aibotid = models.CharField(max_length=36, verbose_name="机器人id", default="", null=False)
+    chatid = models.CharField(max_length=36, verbose_name="聊天id", default="", null=False)
+    chattype = models.CharField(max_length=36, choices=(("group", "群"), ("single", "个人")), default="group",
+                                null=False)
+    chat_from = models.JSONField(default=dict(), null=False, blank=False, db_column="from")
     msgtype = models.CharField(
         max_length=36, choices=(("text", "文字"), ("stream", "数据流")), default="text", null=False
     )
