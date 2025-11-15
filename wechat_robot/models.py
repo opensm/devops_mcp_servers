aebfd52b488adeb2ef904ad910faab1eaf7ba39f
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-15 16:50:51 +0800

    修改序列化结构

--- wechat_robot/models.py
+++ wechat_robot/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class WechatRobotQuestionBase(models.Model):
-    msg_id = models.CharField(max_length=36, unique=True, primary_key=True)
-    aibot_id = models.CharField(max_length=36, name="机器人id", default="", null=False)
-    chat_id = models.CharField(max_length=36, name="聊天id", default="", null=False)
-    chat_type = models.CharField(max_length=36, choices=(("群", "group"),), default="group", null=False)
-    chat_from = models.JSONField(default=dict(), null=False, blank=False)
-    msg_type = models.CharField(
+    msgid = models.CharField(max_length=36, unique=True, primary_key=True)
+    aibotid = models.CharField(max_length=36, name="机器人id", default="", null=False)
+    chatid = models.CharField(max_length=36, name="聊天id", default="", null=False)
+    chattype = models.CharField(max_length=36, choices=(("群", "group"),), default="group", null=False)
+    chat_from = models.JSONField(default=dict(), null=False, blank=False,db_column="from")
+    msgtype = models.CharField(
         max_length=36, choices=(("文字", "text"), ("数据流", "stream")), default="text", null=False
     )
 
--- wechat_robot/serializers.py
+++ wechat_robot/serializers.py
@@ -9,12 +9,12 @@ class WechatRobotQuestionDataSerializer(serializers.ModelSerializer):
 
 
 class WechatRobotQuestionSerializer(serializers.ModelSerializer):
-    msgid = serializers.CharField(max_length=36, source="msg_id")
-    aibotid = serializers.CharField(max_length=36, source="aibot_id")
-    chatid = serializers.CharField(max_length=36, source="chat_id")
-    chattype = serializers.CharField(max_length=36, source="chat_type")
-    # from = serializers.JSONField(source="from")
-    msgtype = serializers.ChoiceField(choices=(("text", "text"), ("stream", "stream")), source="msg_type")
+    # msgid = serializers.CharField(max_length=36, source="msg_id")
+    # aibotid = serializers.CharField(max_length=36, source="aibot_id")
+    # chatid = serializers.CharField(max_length=36, source="chat_id")
+    # chattype = serializers.CharField(max_length=36, source="chat_type")
+    # # from = serializers.JSONField(source="from")
+    # msgtype = serializers.ChoiceField(choices=(("text", "text"), ("stream", "stream")), source="msg_type")
 
     class Meta:
         model = WechatRobotQuestion
