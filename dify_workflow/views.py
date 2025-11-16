3d8bce94a7c1f7f9e9735b18232a016cbc23cd47
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-16 12:44:50 +0800

    修改序列化结构

--- dify_workflow/views.py
+++ dify_workflow/views.py
@@ -2,28 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import ListAPIView, ListCreateAPIView
-
-from dify_workflow.models import Workflow, WorkflowData
-from dify_workflow.serializers import WorkflowSerializer, LLMAgentLogsSerializer
-
-
-class WorkflowDataView(ListCreateAPIView):
-    serializer_class = WorkflowSerializer
-    model = WorkflowData
-
-
-class WorkflowView(ListAPIView):
-    serializer_class = WorkflowSerializer
-    model = Workflow
-
-
-class LLMAgentLogs(ListCreateAPIView):
-    serializer_class = LLMAgentLogsSerializer
-    model = ListCreateAPIView
-
-
-__all__ = [
-    'WorkflowDataView',
-    'WorkflowView',
-    'LLMAgentLogs'
-]
+#
+# from dify_workflow.models import Workflow, WorkflowData
+# from dify_workflow.serializers import WorkflowSerializer, LLMAgentLogsSerializer
+#
+#
+# class WorkflowDataView(ListCreateAPIView):
+#     serializer_class = WorkflowSerializer
+#     model = WorkflowData
+#
+#
+# class WorkflowView(ListAPIView):
+#     serializer_class = WorkflowSerializer
+#     model = Workflow
+#
+#
+# class LLMAgentLogs(ListCreateAPIView):
+#     serializer_class = LLMAgentLogsSerializer
+#     model = ListCreateAPIView
+#
+#
+# __all__ = [
+#     'WorkflowDataView',
+#     'WorkflowView',
+#     'LLMAgentLogs'
+# ]
