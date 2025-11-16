5770101727b65b3ecced063d7cb140b9ff21c764
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-16 23:36:23 +0800

    修改数据序列化方案

--- wechat_robot/serializers.py
+++ wechat_robot/serializers.py
@@ -34,10 +34,13 @@ class WechatRobotQuestionSerializer(serializers.ModelSerializer):
 
     def to_internal_value(self, data):
         logger.debug(f"数据: {data}")
-        stream = data.pop("stream")
+        if "stream" in data:
+            stream = data.pop("stream")
+            data.update({
+                "stream": stream['id'],
+            })
         chat_from = data.pop("from")
         data.update({
-            "stream": stream['id'],
             "chat_from": chat_from['userid']
         })
         return super().to_internal_value(data)
