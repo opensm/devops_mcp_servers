890f9b4575654f81f9ab2b2b303ba6d4e190264f
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-17 00:01:20 +0800

    修改数据序列化方案

--- wechat_robot/views.py
+++ wechat_robot/views.py
@@ -20,22 +20,3 @@ class WechatRobotQuestionView(ListCreateAPIView):
     serializer_class = WechatRobotQuestionSerializer
     parser_classes = [EncryptedDataParser]
     renderer_classes = [EncryptedResponseRenderer]
-
-    # @transaction.atomic  # 可选，保证并发安全
-    # def perform_create(self, serializer):
-    #     if serializer.validated_data.get('msgtype', "") == "stream":
-    #         try:
-    #             stream_id = serializer.validated_data.get('stream', {}).get('id', False)
-    #             robt_instance = WechatRobotQuestion.objects.get(stream=stream_id)
-    #         except WechatRobotQuestion.DoesNotExist:
-    #             logger.error(f"当前请求为流数据，但是未查询到数据: {serializer.validated_data}")
-    #     else:
-    #         logger.debug(f"保存数据: {serializer.validated_data}")
-    #         serializer.save()
-
-        # # 利用唯一索引，先查是否存在
-        # if WechatRobotQuestion.objects.filter(sku_code=sku_code).exists():
-        #     # 已存在 -> 静默跳过，不再 save
-        #     return
-        # # 不存在 -> 真正创建
-        # serializer.save()
