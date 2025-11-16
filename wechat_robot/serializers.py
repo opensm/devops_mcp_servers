9b317b6582f6b982574b3ffddab0a016314b5184
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-16 23:53:20 +0800

    修改数据序列化方案

--- wechat_robot/serializers.py
+++ wechat_robot/serializers.py
@@ -45,23 +45,19 @@ class WechatRobotQuestionSerializer(serializers.ModelSerializer):
         })
         return super().to_internal_value(data)
 
-    def validate_stream(self, attrs):
-        logger.debug(f"验证stream数据: {attrs}")
-        return attrs
-
-    def create(self, validated_data):
-        logger.debug(f"保存数据: {validated_data}")
-        if validated_data.get('msgtype', "") == "stream":
-            try:
-                stream_id = validated_data.get('stream', {}).get('id', False)
-                robt_instance = WechatRobotQuestion.objects.get(stream=stream_id)
-                return robt_instance
-            except WechatRobotQuestion.DoesNotExist:
-                logger.error(f"当前请求为流数据，但是未查询到数据: {validated_data}")
-                raise serializers.ValidationError("当前请求为流数据，但是未查询到数据")
-        else:
-            logger.debug(f"保存数据: {validated_data}")
-            return super().create(validated_data)
+    # def create(self, validated_data):
+    #     logger.debug(f"保存数据: {validated_data}")
+    #     if validated_data.get('msgtype', "") == "stream":
+    #         try:
+    #             stream_id = validated_data.get('stream', {}).get('id', False)
+    #             robt_instance = WechatRobotQuestion.objects.get(stream=stream_id)
+    #             return robt_instance
+    #         except WechatRobotQuestion.DoesNotExist:
+    #             logger.error(f"当前请求为流数据，但是未查询到数据: {validated_data}")
+    #             raise serializers.ValidationError("当前请求为流数据，但是未查询到数据")
+    #     else:
+    #         logger.debug(f"保存数据: {validated_data}")
+    #         return super().create(validated_data)
 
     class Meta:
         model = WechatRobotQuestion
--- wechat_robot/views.py
+++ wechat_robot/views.py
@@ -5,6 +5,7 @@ from wechat_robot.serializers import WechatRobotQuestionDataSerializer, WechatRo
 from common.parsers import EncryptedDataParser
 from common.renderers import EncryptedResponseRenderer
 from common.loger import logger
+from django.db import transaction
 
 
 class WechatRobotQuestionDataView(ListCreateAPIView):
@@ -20,23 +21,21 @@ class WechatRobotQuestionView(ListCreateAPIView):
     parser_classes = [EncryptedDataParser]
     renderer_classes = [EncryptedResponseRenderer]
 
-    # def create(self, request, *args, **kwargs):
-    #     logger.debug(f"创建请求数据: {request.data}")
-    #     try:
-    #         if request.data.get('msgtype', "") == "stream":
-    #             stream_id = request.data.get('stream', {}).get('id', False)
-    #             WechatRobotQuestion.objects.get(stream_id=stream_id)
-    #             return super().create(request, *args, **kwargs)
-    #         else:
-    #             return super().create(request, *args, **kwargs)
-    #     except WechatRobotQuestion.DoesNotExist:
-    #         logger.warning(f"当前请求为流数据，但是未查询到数据: {request.data}")
-    #         return super().create(request, *args, **kwargs)
-    #     except Exception as e:
-    #         logger.error(f"创建数据失败: {str(e)}")
-    #         return super().create(request, *args, **kwargs)
-    #
-    # def perform_create(self, serializer):
-    #     logger.debug(f"保存当前数据: {self.request.data}")
-    #     if self.request.data.get('msgtype', '') == 'text':
-    #         serializer.save()
+    @transaction.atomic  # 可选，保证并发安全
+    def perform_create(self, serializer):
+        if serializer.validated_data.get('msgtype', "") == "stream":
+            try:
+                stream_id = serializer.validated_data.get('stream', {}).get('id', False)
+                robt_instance = WechatRobotQuestion.objects.get(stream=stream_id)
+            except WechatRobotQuestion.DoesNotExist:
+                logger.error(f"当前请求为流数据，但是未查询到数据: {serializer.validated_data}")
+        else:
+            logger.debug(f"保存数据: {serializer.validated_data}")
+            serializer.save()
+
+        # # 利用唯一索引，先查是否存在
+        # if WechatRobotQuestion.objects.filter(sku_code=sku_code).exists():
+        #     # 已存在 -> 静默跳过，不再 save
+        #     return
+        # # 不存在 -> 真正创建
+        # serializer.save()
