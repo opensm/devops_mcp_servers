ac4bf26ab3f2412683a0fc1dfe1610ee7c595403
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-19 21:47:34 +0800

    你好

--- dify_workflow/serializers.py
+++ dify_workflow/serializers.py
@@ -21,7 +21,7 @@ class WorkflowRunDataSerializer(serializers.ModelSerializer):
 
 
 class WorkflowTaskSerializer(serializers.ModelSerializer):
-    data = WorkflowRunDataSerializer()
+    data = WorkflowRunDataSerializer(required=False)
     event = serializers.CharField(max_length=100, default="workflow_started")
 
     class Meta:
