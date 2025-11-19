661f8a03b10a9549ed3046b13431c732107f29ec
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-19 21:08:50 +0800

    你好

--- dify_workflow/serializers.py
+++ dify_workflow/serializers.py
@@ -21,6 +21,9 @@ class WorkflowTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = WorkflowTask
         fields = '__all__'
+        extra_kwargs = {
+            'data': {'validators': []},  # 把默认的 UniqueValidator 摘掉
+        }
 
     def create(self, validated_data):
         """
