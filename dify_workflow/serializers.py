3404756f494639646071c7192dccfd2c8bdbf9c9
ky005509 <yaoshaoqiang@newcowin.com>
2025-11-19 20:55:07 +0800

    你好

--- dify_workflow/serializers.py
+++ dify_workflow/serializers.py
@@ -28,13 +28,21 @@ class WorkflowTaskSerializer(serializers.ModelSerializer):
         用 update_or_create 处理 Category，
         然后同步 products（先删后插）。
         """
+        logger.info(f"保存数据: 1")
         data = validated_data.pop('data', None)
+        logger.info(f"保存数据: 2")
         conversation_id = validated_data.pop('conversation_id')
+        logger.info(f"保存数据: 3")
         message_id = validated_data.pop('message_id')
+        logger.info(f"保存数据: 4")
         task_id = validated_data.pop('task_id')
+        logger.info(f"保存数据: 5")
         workflow_run_id = validated_data.pop('workflow_run_id')
+        logger.info(f"保存数据: 5")
         event = validated_data.pop('event'),
+        logger.info(f"保存数据: 6")
         robot_task = validated_data.pop('robot_task')
+        logger.info(f"保存数据: 7")
         dify_task, _ = WorkflowTask.objects.update_or_create(
             conversation_id=conversation_id,
             message_id=message_id,
@@ -42,6 +50,7 @@ class WorkflowTaskSerializer(serializers.ModelSerializer):
             workflow_run_id=workflow_run_id,
             defaults={"robot_task": robot_task}
         )
+        logger.info(f"保存数据: 8")
         logger.info(f"1开始处理机器人任务 id={dify_task.id}, data={data}")
         if data is not None:
             # 再批量新建
